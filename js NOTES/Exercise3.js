85a586655ce57745d231bd455d087bd674c1f74c
alexpalframan <112865010+alexpalframan@users.noreply.github.com>
2022-09-29 11:55:19 +0100

    exercise3

---
+++ js NOTES/Exercise3.js
@@ -0,0 +1,26 @@
+console.log("Q1 - ")
+console.log("* is an operator, - is an operator, / is an operator, + is an operator. Then -88.8 and 5 are values. 'hello ' is a string")
+console.log("Q2 - ")
+console.log("spam is a variable whereas 'spam' is a string.")
+console.log("Q3 - ")
+console.log("strings, booleans and numbers")
+console.log("Q4 - ")
+console.log("21")
+console.log("Q5 - ")
+console.log("expression 1 = spamspamspam")
+console.log("expression 2 = NaN")
+
+inputFromUser = Number(prompt("please type a number!"))
+if (inputFromUser==1){
+    console.log("Hello");
+} else if (inputFromUser==2){
+    console.log("Howdy")
+} else {
+    console.log("Greetings!")
+}
+
+
+console.log("the error in the first one is it used Console.log (capital c)")
+console.log("the second one uses all caps")
+console.log("the third one uses a capital L")
+console.log("the fourth one doesnt use a . between console and log")
